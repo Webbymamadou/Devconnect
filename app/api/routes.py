16053c040e73eae242c0b36699c0022453da8e6c
Mamadou seck <mamadouseck1357@gmail.com>
2026-05-23 04:57:16 +0000

    modification du fichier de routes dans Api

--- app/api/routes.py
+++ app/api/routes.py
@@ -4,10 +4,10 @@ api_bp = Blueprint('api', __name__)
 from flask import jsonify, abort
 from app.models import Post
 
-@api_bp.route('/posts,', methods=['GET'])
+@api_bp.route('/posts', methods=['GET'])
 def get_posts():
   # On récupère tous les articles en base de données
-  posts = Post.query.order_by(Post.data_posted.desc()).all()
+  posts = Post.query.order_by(Post.date_posted.desc()).all()
   
 # 2. ROUTE : Récupérer tous les articles au format JSON
   # On convertit notre liste d'objets Python en une liste de dictionnaires
@@ -15,9 +15,9 @@ def get_posts():
   for post in posts:
     post_data = {
       'id':post.id,
-      'title': post.title,
+      'title': post.titre,
       'content':post.content,
-      'date_posted':post.date_posted,
+      'date_posted':post.date_posted.isoformat() if post.date_posted else None,
       'author': post.author.username,
       'comments_count': len(post.comments),
     }
@@ -46,7 +46,7 @@ def get_post(post_id):
   # Structure finale de la réponse JSON pour un article unique
   return jsonify({
       'id': post.id,
-      'title': post.title,
+      'title': post.titre,
       'content': post.content,
       'date_posted': post.date_posted.isoformat(),
       'author': post.author.username,
